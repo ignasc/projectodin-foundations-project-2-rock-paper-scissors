f3e1cecbaf897389c854ad493de8461586e3d4f5
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-06 14:37:35 +0000

    Add function to generate computer choice.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("JS file loaded")
+/*
+GET human input
+GET computer input
+DETERMINE who wins
+Return win/lost message
+*/
+
+const ROCK = "rock"
+const PAPER = "paper"
+const SCISSORS = "scissors"
+
+function getComputerInput(){
+    let choice = Math.random()
+    let result = ""
+    if( choice <= 0.33){
+        result = ROCK
+    } else if (choice > 0.66){
+        result = SCISSORS
+    } else {
+        result = PAPER
+    }
+    return result
+}
+
+console.log(getComputerInput())
