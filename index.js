01085dbc6ba05457b72453b6d2fdcc997508e207
Ignas <15948796+ignasc@users.noreply.github.com>
2025-03-06 15:25:37 +0000

    Add basic game logic for a single round of play.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PAPER = "paper"
 const SCISSORS = "scissors"
 
 function getComputerInput(){
+    /*function that generates a choice for computer with rock paper or scissors*/
     let choice = Math.random()
     let result = ""
     if( choice <= 0.33){
@@ -23,10 +24,9 @@ function getComputerInput(){
 }
 
 function getHumanInput(){
+    /*function that lets player select the choice for rock paper or scissors*/
     let choice = parseInt(prompt("1 - Rock\n2 - Paper\n3 - Scissors"))
     let result
-
-    console.log("integer: " + choice)
     
     if(choice > 0 && choice < 4){
         switch(parseInt(choice)){
@@ -39,14 +39,57 @@ function getHumanInput(){
             case 3:
                 result = SCISSORS
                 break;
+            default:
+                result = -1
+                break;
         }
-    } else {
-        console.log("Incorrect option chosen, reload page and try again.")
-        return -1
     }
+
     return result
 
 }
 
-console.log(getComputerInput())
-console.log("Your choice: " + getHumanInput())
+function getGameResults(computerChoice, playerChoice){
+    /*function that returns the result of the game*/
+    switch(computerChoice){
+        case ROCK:
+            if(playerChoice == SCISSORS){
+                return "You lost!"
+            } else if(playerChoice == PAPER){
+                return "You win!"
+            } else {
+                return "It is a tie!"
+            }
+        case PAPER:
+            if(playerChoice == ROCK){
+                return "You lost!"
+            } else if(playerChoice == SCISSORS){
+                return "You win!"
+            } else {
+                return "It is a tie!"
+            }
+        case SCISSORS:
+            if(playerChoice == PAPER){
+                return "You lost!"
+            } else if(playerChoice == ROCK){
+                return "You win!"
+            } else {
+                return "It is a tie!"
+            }
+    }
+
+}
+
+function runGame(){
+    /*function that runs the game*/
+    let computerResult = getComputerInput()
+    let humanResult = getHumanInput()
+    if(humanResult == -1){
+        console.log("Incorrect option chosen, reload page and try again.")
+    } else {
+        console.log("You (" + humanResult + ") vs Computer (" + computerResult + ")\n\n" + getGameResults(computerResult, humanResult))
+    }
+}
+
+/*Run the game*/
+runGame()
